2df6dfddb8d3518a1686f8c1f517171d0932cc46
Benjamin Derksen <b-derksen@mail.de>
2020-07-04 12:58:15 +0200

    cleanup interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import ToDoList from "./pages/ToDoList";
 
-interface todo {
-	title: string;
-	date: Date;
-}
-
 function App() {
 	return (
 		<div className="bg-gray-100 min-h-screen">
